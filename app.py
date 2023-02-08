ad48881cc13360a4d8cdc793c2fef03ce72d4b25
Jiwon Bae <courage2738@gmail.com>
2023-02-08 22:12:00 +0900

    초안 보관용

--- app.py
+++ app.py
@@ -1,93 +1,65 @@
-from flask import Flask, render_template, jsonify, request, session, redirect, url_for
-import jwt
-import datetime
-import hashlib
+from flask import Flask, render_template, request, jsonify, url_for
 from pymongo import MongoClient
-
-SECRET_KEY = 'SPARTA'
 
 app = Flask(__name__)
 
-client = MongoClient('mongodb+srv://test:sparta@cluster0.5dl7iec.mongodb.net/Cluster0?retryWrites=true&w=majority')
-db = client.users
+client = MongoClient('mongodb+srv://toy:toy@cluster0.aowjbzf.mongodb.net/?retryWrites=true&w=majority')
+#db = client.users
 
+db = client['board']
+boards = db['boards']
+#boards.insert_one({"text": 'Test!'})
+
+#test_board = boards.find_one({"text": 'Test!'})
 
 
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    writing_list = list(boards.find())
 
-@app.route('/login')
-def login():
-    return render_template('login.html')
+    return render_template('board_list.html', data=writing_list)
 
-@app.route('/sign')
-def signup():
-    return render_template('sign.html')
+@app.route('/create-page')
+def home2():
+    return render_template('board_writing.html')
 
-# 회원가입
-@app.route('/ToyProject13/sign', methods=['POST'])
-def api_register():
-    id_receive = request.form['id_give']
-    pw_receive = request.form['pw_give']
-    nickname_receive = request.form['nickname_give']
-    pw_hash = hashlib.sha256(pw_receive.encode('utf-8')).hexdigest()
-
-    # 이미 존재하는 아이디면 패스!
-    result = db.user.find_one({'id': id_receive})
-    if result is not None:
-        return jsonify({'result': 'fail', 'msg': '이미 존재하는 ID입니다!'})
-    else:
-        db.user.insert_one({'id': id_receive, 'pw': pw_hash, 'nick': nickname_receive})
-        return jsonify({'result': 'success'})
+@app.route('/board', methods=["GET"])
+def check():
+    print('되나???????')
+    print(request.args.get('_id'))
+    _id = request.args.get('_id')
+    board= boards.find_one({'_id': int(_id)})
+    print(board)
+    return render_template('detail_board.html', board=board)
 
 
-# 로그인
-@app.route('/ToyProject13/login', methods=['POST'])
-def api_login():
-    id_receive = request.form['id_give']
-    pw_receive = request.form['pw_give']
+@app.route("/create", methods=["POST"])
+def create():
+    title_receive = request.form['title']
+    #name_receive= request.form['name']
+    comment_receive = request.form['comment']
+    writing_list = list(boards.find({}, {'_id': False}))
+    count = len(writing_list) + 1
 
-    # 회원가입 때와 같은 방법으로 pw를 암호화합니다.
-    pw_hash = hashlib.sha256(pw_receive.encode('utf-8')).hexdigest()
+    doc = {
+        '_id': count,
+        'title': title_receive,
+        'name': '-',
+        'comment': comment_receive
+    }
 
-    # id, 암호화된pw을 가지고 해당 유저를 찾습니다.
-    result = db.user.find_one({'id': id_receive, 'pw': pw_hash})
-
-    # 찾으면 JWT 토큰을 만들어 발급합니다.
-    if result is not None:
-        # JWT 토큰 생성
-        payload = {
-            'id': id_receive,
-            'exp': datetime.datetime.utcnow() + datetime.timedelta(seconds=100)
-        }
-        token = jwt.encode(payload, SECRET_KEY, algorithm='HS256')
-
-        # token을 줍니다.
-        return jsonify({'result': 'success', 'token': token})
-    # 찾지 못하면
-    else:
-        return jsonify({'result': 'fail', 'msg': '아이디/비밀번호가 일치하지 않습니다.'})
+    boards.insert_one(doc)
+    return render_template('board_list.html')
 
 
-# 보안: 로그인한 사용자만 통과할 수 있는 API
-@app.route('/ToyProject13/isAuth', methods=['GET'])
-def api_valid():
-    token_receive = request.cookies.get('mytoken')
-    try:
-        # token을 시크릿키로 디코딩합니다.
-        payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
-        # payload 안에 id가 들어있습니다. 이 id로 유저정보를 찾습니다.
-        userinfo = db.user.find_one({'id': payload['id']}, {'_id': 0})
-        return jsonify({'result': 'success', 'nickname': userinfo['nick']})
-    except jwt.ExpiredSignatureError:
-        # 위를 실행했는데 만료시간이 지났으면 에러가 납니다.
-        return jsonify({'result': 'fail', 'msg': '로그인 시간이 만료되었습니다.'})
-    except jwt.exceptions.DecodeError:
-        # 로그인 정보가 없으면 에러가 납니다!
-        return jsonify({'result': 'fail', 'msg': '로그인 정보가 존재하지 않습니다.'})
+
+@app.route("/writing", methods=["POST"])
+def writing_get():
+    writingpage = list(boards.find())
+    return jsonify(writingpage)
+
 
 
 if __name__ == '__main__':
-    app.run('0.0.0.0', port=5000, debug=True);
+    app.run('0.0.0.0', port=5000, debug=True)
